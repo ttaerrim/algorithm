96a637a1f06e1956867f95e73ba5ad19590e32d0
ttaerrim <ltr0121@naver.com>
2021-07-22 13:55:18 +0900

    로그 파일 재정렬

--- algorithm-interview/3.py
+++ algorithm-interview/3.py
@@ -12,8 +12,5 @@ class Solution:
         return letter+digit
 
 
-s = Solution()
-print(s.reorderLogFiles(["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]))
-print(s.reorderLogFiles(["a1 9 2 3 1","g1 act car","zo4 4 7","ab1 off key dog","a8 act zoo","a2 act car"]))
 
         
