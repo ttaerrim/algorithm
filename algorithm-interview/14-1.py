515e08c1cf54c3e6fe992dca8d766a6dc34d2be6
ttaerrim <ltr0121@naver.com>
2021-08-18 15:40:34 +0900

    21. 두 정렬 리스트의 병합

--- algorithm-interview/14-1.py
+++ algorithm-interview/14-1.py
@@ -1,27 +1,29 @@
-# Definition for singly-linked list.
 from typing import Optional
 
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
 class Solution:
     def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         
+        # 책보다 조금 더 쉬운 재귀 구조 
+
+        if l1 is None:
+            return l2
+        if l2 is None:
+            return l1
+        
         dummy = ListNode()
         head = dummy
-        while l1 and l2:
-            if l1.val < l2.val:
-                head.next = l1
-                l1 = l1.next
-            else:
-                head.next = l2
-                l2 = l2.next
-            head = head.next
         
-        if l1 is None:
-            head.next = l2
-        elif l2 is None:
+        if l1.val < l2.val:
             head.next = l1
+            head.next.next = self.mergeTwoLists(l1.next, l2)
+        else:
+            head.next = l2
+            head.next.next = self.mergeTwoLists(l1, l2.next)
+            
 
         return dummy.next
