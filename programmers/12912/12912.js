6c126e56dbd48a13e62b58913b1237f6d97e387e
ttaerrim <ltr0121@naver.com>
2022-01-07 14:19:08 +0900

    programmers 두 정수 사이의 합

---
+++ programmers/12912/12912.js
@@ -0,0 +1,9 @@
+// 두 정수 사이의 합
+function solution(a, b) {
+  var answer = 0;
+
+  for (var i = Math.min(a, b); i <= Math.max(a, b); i++) {
+    answer += i;
+  }
+  return answer;
+}
---
+++ programmers/12912/12912.py
@@ -0,0 +1,15 @@
+# 두 정수 사이의 합
+def solution(a, b):
+    answer = 0
+    min, max = 0, 0
+    if a > b:
+        a, b = b, a
+        
+    for i in range(a, b+1):
+        
+        answer += i
+    return answer
+
+
+def solution2(a, b):
+    return sum(range(min(a, b), max(a, b)+1))
