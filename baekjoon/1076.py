36aea89f3c6cce2e521c72b0557125cad5719ddd
ttaerrim <ltr0121@naver.com>
2022-03-07 18:07:23 +0900

    [Refactor] 백준 1076 저항

--- baekjoon/1076.py
+++ baekjoon/1076.py
@@ -1,12 +1,6 @@
 # 저항
-import math
+color = ['black', 'brown', 'red', 'orange', 'yellow',
+         'green', 'blue', 'violet', 'grey', 'white']
+first, second, third = [color.index(input()) for i in range(3)]
 
-
-color = {'black': 1, 'brown': 10, 'red': 100, 'orange': 1000, 'yellow': 10000,
-         'green': 100000, 'blue': 1000000, 'violet': 10000000, 'grey': 100000000, 'white': 1000000000}
-first_color = input()
-second_color = input()
-third_color = input()
-
-print((int(math.log10(color[first_color])) *
-      10+int(math.log10(color[second_color])))*color[third_color])
+print((first*10+second)*10**third)
