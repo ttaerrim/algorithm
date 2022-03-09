d3fc298d7c9ce2420eb654ba9ac6c323a9473819
ttaerrim <ltr0121@naver.com>
2022-03-09 19:07:06 +0900

    [Refac] 백준 1157 단어 공부

--- baekjoon/1157.py
+++ baekjoon/1157.py
@@ -1,14 +1,7 @@
 # 단어 공부
 from collections import Counter
 
-
-def find_max(word):
-    if len(word) == 1 or word[0][1] > word[1][1]:
-        return word[0][0]
-    else:
-        return "?"
-
-
 word = input().upper()
 word_counter = Counter(word).most_common(2)
-print(find_max(word_counter))
+print(word_counter[0][0] if len(word_counter) ==
+      1 or word_counter[0][1] > word_counter[1][1] else '?')
