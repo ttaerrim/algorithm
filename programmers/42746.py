8b7ff2173c0d6032bb7d9d994ba6081fa9488909
ttaerrim <ltr0121@naver.com>
2022-03-16 23:33:19 +0900

    [Refactor] 프로그래머스 42746 가장 큰 수

--- programmers/42746.py
+++ programmers/42746.py
@@ -1,6 +1,5 @@
 # 가장 큰 수
 def solution(numbers):
     numbers.sort(key=lambda x: ((str(x)*4)[:4]), reverse=True)
-    answer = "".join(map(str, numbers))
-    answer = answer if int(answer) != 0 else "0"
+    answer = str(int("".join(map(str, numbers))))
     return answer
