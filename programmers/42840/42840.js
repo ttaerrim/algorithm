7526ed15877af7c0b98179f60e939126ab15dd84
ttaerrim <ltr0121@naver.com>
2023-05-20 23:00:00 +0100

    [Update] 프로그래머스 - 모의고사 (js)

--- programmers/42840/42840.js
+++ programmers/42840/42840.js
@@ -28,3 +28,30 @@ function solution(answers) {
 
   return answer;
 }
+
+function solution(answers) {
+  const supojas = [
+    [1, 2, 3, 4, 5],
+    [2, 1, 2, 3, 2, 4, 2, 5],
+    [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
+  ];
+
+  const answerCount = [0, 0, 0];
+  const answer = [];
+
+  for (let i in answers) {
+    supojas.forEach((_, idx) => {
+      if (answers[i] === supojas[idx][i % supojas[idx].length]) {
+        answerCount[idx] += 1;
+      }
+    });
+  }
+
+  for (let i = 0; i < answerCount.length; i++) {
+    if (answerCount[i] === Math.max(...answerCount)) {
+      answer.push(i + 1);
+    }
+  }
+
+  return answer;
+}
