2835f898b90c5c36ebeb25c67373782d659cbb82
ttaerrim <ltr0121@naver.com>
2023-12-28 15:26:21 +0900

    [Update] 백준 - 세수정렬 (python)

--- baekjoon/2752.py
+++ baekjoon/2752.py
@@ -3,3 +3,32 @@ num_array.sort()
 for i in range(len(num_array)):
 
     print(num_array[i], end=" ")
+
+#################### 다른 풀이 
+
+a, b, c = map(int, input().split())
+maxNum = max([a, b, c])
+minNum = min([a, b, c])
+
+def middle(array):
+    for i in array:
+        if (i != max(array) and i != min(array)):
+            middle = i
+    return middle
+
+
+middle = middle([a, b, c])
+print(minNum, middle, maxNum)
+
+#################### 숏 코딩
+
+print(*sorted(map(int, input().split())))
+
+#################### 다른 풀이 2
+
+a, b, c = map(int, input().split())
+maxNum = max([a, b, c])
+minNum = min([a, b, c])
+middleNum = a + b + c - maxNum - minNum
+
+print(minNum, middleNum, maxNum)
