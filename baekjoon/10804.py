0336254160e60f988d0dd22776d0e0a842542c82
ttaerrim <ltr0121@naver.com>
2023-12-30 18:57:49 +0900

    [Update] 백준 - 카드 역배치 추가 (python)

--- baekjoon/10804.py
+++ baekjoon/10804.py
@@ -20,3 +20,14 @@ for i in range(10):
 
 for i in range(len(list)):
     print(list[i], end=" ")
+
+
+################## 코드 줄이기
+lists = [i for i in range(1, 21)]
+
+for i in range(10):
+    a, end = map(int, input().split())
+    start = a - 1
+    lists = lists[:start] + lists[start:end][::-1] + lists[end:]
+
+print(' '.join(map(str, lists)))
