97656f96a4af4e5c607cfd73bc11ed927e37424f
ttaerrim <ltr0121@naver.com>
2024-01-01 20:32:40 +0900

    [Update] 백준 - 개수 세기 추가 (python)

--- baekjoon/10807.py
+++ baekjoon/10807.py
@@ -9,3 +9,14 @@ for i in range(n):
 
 nIdx = v if v >= 0 else v * -1 + 100
 print(nums[nIdx])
+
+################# 100을 더해 음수를 더 쉽게 다루자
+n = int(input())
+lists = list(map(int, input().split()))
+v = int(input())
+nums = [0 for _ in range(201)]
+
+for i in range(n):
+    nums[lists[i]+100] += 1
+
+print(nums[v+100])
