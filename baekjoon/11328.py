49f1dd1da6fa2e2a07cab4bb22f46dbf4531285f
ttaerrim <ltr0121@naver.com>
2024-01-01 21:03:08 +0900

    [Update] 백준 - Strfry 추가 풀이(python)

--- baekjoon/11328.py
+++ baekjoon/11328.py
@@ -21,3 +21,23 @@ for i in range(n):
         continue
     else:
         print('Possible')
+
+# 첫 번째 단어는 조회하며 알파벳 배열에서 1을 더하고
+# 두 번째 단어는 조회하며 알파벳 배열에서 1을 뺀다
+        
+n = int(input())
+flag = False
+for i in range(n):
+    alpha = [0 for _ in range(26)]
+    str1, str2 = map(str, input().split())
+    for j in str1:
+        idx1 = ord(j) - 97
+        alpha[idx1] += 1
+    for k in str2:
+        idx2 = ord(k) - 97
+        alpha[idx2] -= 1
+
+    if (alpha.count(0) == 26):
+        print('Possible')
+    else:
+        print('Impossible')
