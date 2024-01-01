c8501673076d13a2bad36b330304b5fc4be89e8a
ttaerrim <ltr0121@naver.com>
2024-01-01 17:55:42 +0900

    [Update] 백준 - 숫자의 개수 추가 풀이 (python)

--- baekjoon/2577.py
+++ baekjoon/2577.py
@@ -10,3 +10,18 @@ for i in range(len(str(multiple))):
 
 for i in range(len(numbers)):
     print(numbers[i])
+
+
+################### 다른 풀이
+t = 1
+for _ in range(3):
+    t *= int(input())
+
+numbers = [0 for _ in range(10)]
+
+
+while (t > 0):
+    numbers[t%10] += 1
+    t=t//10
+
+print("\n".join(str(x) for x in numbers))
