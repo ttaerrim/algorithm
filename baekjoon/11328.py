d99127c908f48b8d9c3c388728cd639301262915
ttaerrim <ltr0121@naver.com>
2024-01-01 20:57:36 +0900

    [Update] 백준 - Strfry (python)

---
+++ baekjoon/11328.py
@@ -0,0 +1,23 @@
+n = int(input())
+flag = False
+for i in range(n):
+    alpha = [0 for _ in range(26)]
+    str1, str2 = map(str, input().split())
+    if(len(str1)!=len(str2)):
+        print('Impossible')
+        continue
+    for j in str1:
+        idx1 = ord(j) - 97
+        alpha[idx1] += 1
+    for k in str2:
+        idx2 = ord(k) - 97
+        if(alpha[idx2] == 0):
+            print('Impossible')
+            flag = True
+            break
+        alpha[idx2] -= 1
+    if(flag):
+        flag = False
+        continue
+    else:
+        print('Possible')
