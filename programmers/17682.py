6d959e20af61f64dcca7f1c75cc5991d3e591190
ttaerrim <ltr0121@naver.com>
2024-01-26 13:17:12 +0900

    [Update] 프로그래머스 - 17682 다트 게임 추가 풀이

--- programmers/17682.py
+++ programmers/17682.py
@@ -22,7 +22,34 @@ def solution(dartResult):
             scores[-1] = scores[-1] * -1
         idx += 1
         
-    print(scores)
+    return sum(scores)
+
+
+def solution2(dartResult):
+    scores = []
+    idx = 0
+    bonus = {'S':1, 'D':2, 'T':3}
+    while idx < len(dartResult):
+        score = 0
+        while dartResult[idx].isdigit():
+            score *= 10
+            score += int(dartResult[idx])
+            idx += 1
+        # 여기서 위처럼 if문 거치지 않고 풀 수 있음
+        score = score ** bonus[dartResult[idx]]
+        scores.append(score)
+        idx += 1
+        
+        if idx < len(dartResult) and dartResult[idx] == '*':
+            scores[-1] *= 2
+            if(len(scores)>1):
+                scores[-2] = scores[-2] * 2
+            idx += 1
+        elif idx < len(dartResult) and dartResult[idx] == '#':
+            scores[-1] = scores[-1] * -1
+            idx += 1
+        
+        
     return sum(scores)
 
 # 문자열을 숫자로 변환하는 함수
