cf9a3e90164bd57a9de1a57c9bc25c7a4d4879be
ttaerrim <ltr0121@naver.com>
2024-02-06 00:30:28 +0900

    [Update] 프로그래머스 - 42587 프로세스 다른 풀이 (python)

--- programmers/42587.py
+++ programmers/42587.py
@@ -1,3 +1,7 @@
+# 스택/큐 > 프로세스
+# https://school.programmers.co.kr/learn/courses/30/lessons/42587
+
+
 from collections import deque
 
 def solution(priorities, location):
@@ -26,3 +30,23 @@ def solution(priorities, location):
 
         
     return answer.index(location) + 1
+
+
+# 다른 풀이
+# [[파이썬 기초 코테 꿀팁] deque 알아보기](https://www.youtube.com/watch?v=Ckua6-QH5tU&list=PLDFJ6OuoOt_whs1SJ3MS5HXK_52r57qIB&index=6)
+
+def solution2(priorities, location):
+    waiting = deque(enumerate(priorities))
+    sorting = sorted(priorities)
+    
+    order = 1
+    while waiting:
+        idx, process = waiting.popleft()
+        if process < sorting[-1]:
+            waiting.append((idx, process))
+        else:
+            if idx == location:
+                return order # pop 한 것을 배열에 저장하지 않고도 order를 사용해서 정답을 리턴할 수 있다
+            sorting.pop()
+            order += 1
+        
