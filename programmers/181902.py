450fbb6744431a3de843fa2e985d87b35aa548e4
ttaerrim <ltr0121@naver.com>
2024-02-07 20:10:24 +0900

    [Update] 프로그래머스 - 181902 문자 개수 세기 추가 풀이  (python)

--- programmers/181902.py
+++ programmers/181902.py
@@ -14,3 +14,19 @@ def solution(my_string):
         alphabets[s] += 1
     
     return [alphabets[k] for k in alphabets]
+
+
+# 다른 풀이 2
+
+def solution(my_string):
+    letters = defaultdict(int) # 처음에 굳이 A~Za~z 딕셔너리를 만들지 않아도 됨 
+    for s in my_string: 
+        letters[s] += 1 
+    
+    answer = []
+    for x in range(ord('A'), ord('Z')+1):
+        answer.append(letters[chr(x)])
+    for x in range(97, 123):
+        answer.append(letters[chr(x)])
+    
+    return answer
