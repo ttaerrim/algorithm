4b2cdd28db3037942b13622f2c5621578fa5d51b
ttaerrim <ltr0121@naver.com>
2024-02-21 12:04:20 +0900

    [Update] 프로그래머스 - 12933 정수 내림차순으로 배치하기 추가 풀이 (python)

--- programmers/12933.py
+++ programmers/12933.py
@@ -4,3 +4,17 @@
 def solution(n):
     n2str = [int(s) for s in str(n)]
     return int("".join(sorted(map(str, n2str), reverse=True)))
+
+# 다른 풀이 2
+def solution(n):
+    lst = []
+    while n:
+        _, v = divmod(n, 10)
+        lst.append(v)
+        n //= 10
+    lst.sort(reverse=True)
+    answer = 0
+    for n in lst:
+        answer *= 10
+        answer += n
+    return answer
